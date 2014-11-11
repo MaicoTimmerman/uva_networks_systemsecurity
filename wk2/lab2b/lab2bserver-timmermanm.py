386f48993469f98cf2a8d61e0c6f4da34621f79f
Maico Timmerman <maico.timmerman@gmail.com>
2014-11-11 11:47:54 +0100

    Fixed all the non-pep8 crap

--- wk2/lab2b/lab2bserver-timmermanm.py
+++ wk2/lab2b/lab2bserver-timmermanm.py
@@ -1,20 +1,25 @@
 ## Netwerken en Systeembeveiliging Lab 2B - Chat Room (Server)
-## NAME:
-## STUDENT ID:
+## NAME: Maico Timmerman
+## STUDENT ID: 10542590
 
-def serve(port):
-	"""
-	Chat server entry point.
-	port: The port to listen on.
-	"""
-	pass
+
+class ChatServer():
+
+    def __init__():
+
+    def serve(port):
+        """
+        Chat server entry point.
+        port: The port to listen on.
+        """
+        pass
 
 
 ## Command line parser.
 if __name__ == '__main__':
-	import sys, argparse
-	p = argparse.ArgumentParser()
-	p.add_argument('--port', help='port to listen on', default=12345, type=int)
-	args = p.parse_args(sys.argv[1:])
-	serve(args.port)
-
+    import sys
+    import argparse
+    p = argparse.ArgumentParser()
+    p.add_argument('--port', help='port to listen on', default=12345, type=int)
+    args = p.parse_args(sys.argv[1:])
+    serve(args.port)
